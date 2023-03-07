4a21b595b70827cec72a5d09aee5f39712b97dae
Aly Mamdouh <Aly.Mamdouh@InteractTS.com>
2023-03-07 22:05:38 +0200

    Add project files.

---
+++ ConsoleFluentEmailDemo/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace ConsoleFluentEmailDemo
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
